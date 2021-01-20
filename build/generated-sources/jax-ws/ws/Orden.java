aab6a981bd805321b73fb3cb57d59be9ee683cbd
Angel Perez <agpc_din@hotmail.com>
2021-01-20 17:40:26 -0600

    primer commit

---
+++ build/generated-sources/jax-ws/ws/Orden.java
@@ -0,0 +1,147 @@
+
+package ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Clase Java para orden complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="orden"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="cantidad" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="fecha" type="{http://www.w3.org/2001/XMLSchema}dateTime" minOccurs="0"/&gt;
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int" minOccurs="0"/&gt;
+ *         &lt;element name="idCliente" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="idProducto" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "orden", propOrder = {
+    "cantidad",
+    "fecha",
+    "id",
+    "idCliente",
+    "idProducto"
+})
+public class Orden {
+
+    protected int cantidad;
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar fecha;
+    protected Integer id;
+    protected int idCliente;
+    protected int idProducto;
+
+    /**
+     * Obtiene el valor de la propiedad cantidad.
+     * 
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * Define el valor de la propiedad cantidad.
+     * 
+     */
+    public void setCantidad(int value) {
+        this.cantidad = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad fecha.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getFecha() {
+        return fecha;
+    }
+
+    /**
+     * Define el valor de la propiedad fecha.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setFecha(XMLGregorianCalendar value) {
+        this.fecha = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad id.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Integer }
+     *     
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * Define el valor de la propiedad id.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Integer }
+     *     
+     */
+    public void setId(Integer value) {
+        this.id = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad idCliente.
+     * 
+     */
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    /**
+     * Define el valor de la propiedad idCliente.
+     * 
+     */
+    public void setIdCliente(int value) {
+        this.idCliente = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad idProducto.
+     * 
+     */
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    /**
+     * Define el valor de la propiedad idProducto.
+     * 
+     */
+    public void setIdProducto(int value) {
+        this.idProducto = value;
+    }
+
+}
